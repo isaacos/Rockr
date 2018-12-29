6556fe9cda439ddeb00ad1c79bd4663a0a24517f
isaac <oseasisaac91@gmail.com>
2018-12-28 23:14:06 -0500

    search bar for musician finished

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -8,7 +8,9 @@ class MusiciansController < ApplicationController
   end
 
   def create
-    @musician = Musician.create(musician_params)
+    @musician = Musician.new(musician_params)
+    @musician.instrument_capitalize
+    @musician.save
     redirect_to musician_path(@musician)
   end
 
@@ -17,14 +19,23 @@ class MusiciansController < ApplicationController
   end
 
   def index
-    @musicians = Musician.all
+    if params[:instrument_search] && params[:genre][:genre_search] == ""
+      @musicians = Musician.where('instrument Like ?', "%#{params[:instrument_search]}")
+    elsif params[:instrument_search] == "" && params[:genre][:genre_search]
+      @musicians = Musician.genre_type(params[:genre][:genre_search])
+    elsif params[:instrument_search] && params[:genre][:genre_search]
+      @musicians = Musician.genre_type(params[:genre][:genre_search]).where('instrument Like ?', "%#{params[:instrument_search]}")
+    else
+
+      @musicians = Musician.all
+    end
   end
 
 
   private
 
   def musician_params
-    params.require(:musician).permit(:name, :instrument, :description, :time_for_music)
+    params.require(:musician).permit(:name, :instrument, :description, :time_for_music, :instrument_search, :genre_search, :genre_ids => [])
   end
 
 end
--- app/models/musician.rb
+++ app/models/musician.rb
@@ -3,4 +3,13 @@ class Musician < ApplicationRecord
   has_many :musician_bands
   has_many :genres, through: :genre_musicians
   has_many :bands, through: :musician_bands
+
+  def instrument_capitalize
+    instrument = self.instrument.split.map(&:capitalize).join(' ')
+    self.instrument = instrument
+  end
+
+  def self.genre_type(genre_name)
+    Genre.find_by(genre_name: genre_name).musicians
+  end
 end
